740cbdb9cc335df5157c279f7b66bfabfcfa3dce
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2011-11-09 22:29:19 -0200

    change build system to cmake

---
+++ run_tests.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+gcc test/run_all_tests.c test/test_http_messages.c test/test_hash.c test/test_iterator.c src/http_messages.c src/hash.c src/iterator.c -lcunit -o run_all_tests &&
+chmod +x ./run_all_tests &&
+./run_all_tests
